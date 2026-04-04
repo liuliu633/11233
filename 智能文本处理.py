cad774c83731c0f84a699436e5c94979e3b43f84
lyx315-good <snnhka@outlook.com>
2026-04-04 16:10:01 +0800

    Update 智能文本处理(版本一).py
    1.能统计词:统计个数，对文本进行分词统计，统计每个单词出现的次数

--- 智能文本处理.py
+++ 智能文本处理.py
@@ -1,13 +1,11 @@
-#第一步：输入任意文本
 print("智能文本处理系统")
 import jieba
 import string
 from zhon.hanzi import punctuation
 import re
-#input()获取输入的文本，strip()去除首尾多余空格
+#input()
 text = input("请输入你想要统计的文本：").strip()
 
-#判断：若用户没输入内容，提示并退出
 if not text:
     print("错误：你还没输入任何文本！")
 else:
@@ -26,31 +24,26 @@ else:
     print(word_list)
     print("-" * 50)
 
-#第三步：用线性表统计词频
-#初始化两个列表：一个存不重复的单词，一个存对应频次
 unique_words = []
 word_counts = []
 for word in word_list:
-    #单词不在unique里，添加进去，频次设为一
     if word not in unique_words:
         unique_words.append(word)
         word_counts.append(1)
-    #若已经存在，找到位置，频次加一
+
     else:
-        index = unique_words.index(word) #index查找词第一次出现
+        index = unique_words.index(word) 
         word_counts[index] += 1
 
-    #第四步：打印统计结果
 print("\n词频统计结果：")
-print("单词\t\t频次") #\t：制表符，让格式整齐
+print("单词\t\t频次")
 print("-" * 20)
 #遍历两个列表，打印每个单词和对应频次
 for i in range(len(unique_words)):
     print(f"{unique_words[i]}\t\t{word_counts[i]}")
 
-#统计总结
 print("-" * 20)
-total_words = sum(word_counts) #总单词数
-different_words = len(unique_words) #不同单词数
+total_words = sum(word_counts) 
+different_words = len(unique_words) 
 print(f"总单词数量：{total_words}")
 print(f"不同单词数量：{different_words}")
